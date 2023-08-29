e6bfb1d26d3c1ce8ab10cfe7a051a9159074d209
Sylvia T <sylviarosemarie@gmail.com>
2023-08-29 16:02:18 -0500

    const brain = broken; const mind = boggled;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-import "./App.css"
+import "./App.css";
+import { useState, useRef } from "react";
 
 export const App = () => {
-  return <div>Hello World!</div>
-}
+  const [inputText, setInputText] = useState("New One Liner");
+
+  return (
+    <>
+      <article>
+        <div className="app-heading">
+          <h1 className="app-heading-text">Chuckle Checklist</h1>
+        </div>
+        <section className="joke-add-form">
+          <input
+            className="joke-input"
+            type="text"
+            value={inputText}
+            // placeholder="New One Liner"
+            required
+            onChange={(event) => {
+              // What's the value of event?
+            }}
+          />
+          <input className="joke-input-submit" type="submit" value="Add" />
+        </section>
+      </article>
+    </>
+  );
+};
